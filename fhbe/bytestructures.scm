379996e8140c1b5231fee67ac3173f0d1ceda6c5
Matthew Wette <mwette@alumni.caltech.edu>
2025-12-21 14:38:47 -0800

    bytestructures seems to work; bstruct needs a lot of work

---
+++ fhbe/bytestructures.scm
@@ -0,0 +1,145 @@
+;;; bytestructures.scm
+
+;; Copyright (C) 2025 Matthew Wette
+;;
+;; This library is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU Lesser General Public
+;; License as published by the Free Software Foundation; either
+;; version 3 of the License, or (at your option) any later version.
+;;
+;; This library is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Lesser General Public License for more details.
+;;
+;; You should have received a copy of the GNU Lesser General Public License
+;; along with this library; if not, see <http://www.gnu.org/licenses/>
+
+;;; Code:
+
+(define-module (fhbe bytestructures)
+  #:export (backend)
+  #:use-module (bytestructures guile)
+  #:use-module ((system foreign) #:prefix ffi:)
+  #:use-module (nyacc lang c99 fh-utils))
+
+(define (header)
+  `(begin
+     (use-modules (bytestructures guile))
+     (define (arg->number arg)
+       (cond ((number? arg) arg)
+             ((bytestructure? arg) (bytestructure-ref arg))
+             (else (error "bytestructure-arg->number: bad arg:" arg))))
+     (define* (arg->pointer arg #:optional hint)
+       (cond ((ffi:pointer? arg) arg)
+             ((string? arg) (ffi:string->pointer arg))
+             ((equal? 0 arg) ffi:%null-pointer)
+             ((bytestructure? arg)
+              (let* ((desc (bytestructure-descriptor arg))
+                     (meta (bytestructure-descriptor-metadata desc)))
+                (cond
+                 ((pointer-metadata? meta)
+                  (ffi:make-pointer (bytestructure-ref arg)))
+                 ((or (vector-metadata? meta)
+                      (struct-metadata? meta)
+                      (union-metadata? meta))
+                  (call-with-values
+                      (lambda () (bytestructure-unwrap arg))
+                    (lambda (bv ix ds)
+                      (ffi:make-pointer (+ (ffi:bytevector->pointer bv) ix)))))
+                 (else (error "bytestrucgture-arg->pointer; bad arg:" arg)))))
+             (else (error "bytestructure-arg->pointer: bad arg:" arg))))
+     (define (extern-ref obj) (bytestructure-ref obj '*))
+     (define (extern-set! obj val) (bytestructure-set! obj '* val))
+     (eval-when (expand load eval) (define backend 'bytestructures))))
+
+(define (trailer defs)
+  (let ((sym->val (or (assq-ref defs 'sym->val) '(const #f))))
+    `(define (unwrap-enum arg)
+       (cond
+        ((number? arg) arg)
+        ((symbol? arg) (,sym->val arg))
+        ((bytestructure? arg) (bytestructure-ref arg))
+        (else (error "type mismatch"))))))
+
+(define (no-base name)
+  (fherr/once "no backend type for ~a" name))
+
+(define (base name)
+  (case name
+    ((char) 'int8)
+    ((signed-char) 'int8)
+    ((unsigned-char) 'uint8)
+    ((short) 'short)
+    ((unsigned-short) 'short)
+    ((int) 'int)
+    ((unsigned) 'unsigned-int)
+    ((long) 'long)
+    ((unsigned-long) 'unsigned-long)
+    ((long-long) 'long-long)
+    ((unsigned-long-long) 'unsigned-long-long)
+    ((float) 'float)
+    ((double) 'double)
+    ((int8_t) 'int8)
+    ((uint8_t) 'uint8)
+    ((int16_t) 'int16)
+    ((uint16_t) 'uint16)
+    ((int32_t) 'int32)
+    ((uint32_t) 'uint32)
+    ((int64_t) 'int64)
+    ((uint64_t) 'uint64)
+    ((size_t) 'size_t)
+    ((ssize_t) 'ssize_t)
+    ((ptrdiff_t) 'ptrdiff_t)
+    ((intptr_t) 'intptr_t)
+    ((uintptr_t) 'uintptr_t)
+    ((_Bool bool) 'int8)
+    ((wchar_t) 'uint32)
+    ((char16_t) 'uint16)
+    ((char32_t) 'uint32)
+    ((long-double) (no-base name))
+    ((_Float16) (no-base name))
+    ((_Float128) (no-base name))
+    ((float-_Complex) 'complex64)
+    ((double-_Complex) 'complex128)
+    ((long-double-_Complex) (no-base name))
+    ((__int128) (no-base name))
+    ((unsigned-__int128) (no-base name))
+    (else (no-base name))))
+
+(define (array type dim)
+  `(bs:vector ,dim ,type))
+
+(define* (struct fields #:optional packed)
+  (let ((flds (map (lambda (f)
+                     (if (pair? (cadr f)) (cons (car f) (cadr f)) f))
+                   fields)))
+    `(bs:struct (list ,@flds))))
+
+(define (bitfield type size)
+  (list type size))
+
+
+(define backend
+  (make-fh-backend
+   header                               ; header
+   trailer                              ; trailer
+   base                                 ; base
+   array                                ; array
+   (lambda (type)                       ; pointer
+     `(bs:pointer ,type)) 
+   (lambda* (flds #:optional packed)    ; struct
+     (if packed `(bs:struct (list ,@flds) #t) `(bs:struct (list ,@flds))))
+   bitfield                             ; bitfield
+   (lambda (flds)                       ; union
+      `(bs:union (list ,@flds)))
+   (lambda (pr->pc pc->pr)              ; function
+     ''void)
+   (lambda* (alist #:optional packed)   ; enum
+     'int)
+   (lambda (name type)                  ; deftype
+     `(define-public ,name ,type))
+   (lambda* (type #:optional value)
+     (if value `(bytestructure ,type ,value) `(bytestructure ,type)))))
+
+;; --- last line ---
